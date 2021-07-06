cce9b05596ca170674c4d4d9775b6930b151855d
devika-s00 <84584568+devika-s00@users.noreply.github.com>
2021-07-06 11:59:43 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -199,7 +199,9 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count=0;
   while (p!=NULL) {
+  count++;
     printf("%-20s : %10s\n", p->name, p->phone);
     p=p->next;
   }
