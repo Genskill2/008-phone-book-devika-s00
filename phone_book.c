09923a0bb0ff635e09b9a162feaa279bc22f8a83
devika-s00 <84584568+devika-s00@users.noreply.github.com>
2021-07-06 14:17:05 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -62,13 +62,13 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    //printf("NOT IMPLEMENTED!\n"); /* TBD  */
-   if(argc!=3) {
-     print_usage("Improper arguments for delete",argv[0]);
-     exit(1);
-   }
-  FILE *fp=open_db_file();
-  char *name = argv[2];
+   // printf("NOT IMPLEMENTED!\n"); /* TBD  */
+    if (argc != 3) {
+      print_usage("Improper arguments for delete", argv[0]);
+      exit(1);
+    }
+      FILE *fp = open_db_file();
+    char *name = argv[2];
     if (!search(fp, name)) {
       printf("no match\n");
       fclose(fp);
@@ -107,7 +107,7 @@ FILE *open_db_file() {
   
 void free_entries(entry *p) {
   /* TBD */
-   entry *temp=p;
+ entry *temp=p;
   entry *tmp_nxt;
   while(temp!=NULL){
     tmp_nxt=temp->next;
@@ -152,20 +152,17 @@ entry *load_entries(FILE *fp) {
   entry *tmp = NULL;
   /* Description of %20[^,\n]
      % is the start of the specifier (like %s, %i etc.)
-
      20 is the maximum number of characters that this will take. We
         know that names and phone numbers will be 20 bytes maximum so
         we limit it to that. %20s will read in 20 character strings
         (including the , to separate the name and phone number. That's
         why we use
-
     [^,\n] Square brackets are used to indicate a set of allowed
            characters [abc] means only a, b, or c. With the ^, it's
            used to specify a set of disallowed characters. So [^abc]
            means any character *except* a, b, or c. [^,] means any
            character except a , [^,\n] means any character except a
            comma(,) or a newline(\n).
-
     %20[^,\n] will match a string of characters with a maximum length
      of 20 characters that doesn't have a comma(,) or a newline(\n).
   */        
@@ -201,13 +198,13 @@ void list(FILE *db_file) {
   entry *base = p;
   int count=0;
   while (p!=NULL) {
-  count++;
+    count++;
     printf("%-20s : %10s\n", p->name, p->phone);
     p=p->next;
   }
   /* TBD print total count */
-  printf("Total entries: %d\n",count);
-  free_entries(base);
+  printf("Total entries :  %d\n",count);
+   free_entries(base);
 }
 int search(FILE *db_f,char *name){
   entry *p=load_entries(db_f);
@@ -244,17 +241,18 @@ int delete(FILE *db_file, char *name) {
       */
 
       /* TBD */
-     if(p==base) {
-     base=p->next;
-     free(p);
-     deleted=1;
-    }
-    else {
-    prev->next=p->next;
-    free(p);
-    p=prev->next;
-    deleted=1;
-    continue;}
+      if(p==base){
+        base=p->next;
+        free(p);
+        deleted=1;
+      }
+      else{
+        prev->next=p->next;
+        free(p);
+        p=prev->next;
+        deleted=1;
+        continue;
+      }
     }
     prev=p;
     p=p->next;
@@ -263,3 +261,6 @@ int delete(FILE *db_file, char *name) {
   free_entries(base);
   return deleted;
 }
+  
+
+ 
